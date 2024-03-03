0db6000aaac250824266ac37451f43ce272d80a3
Loong <wangl.cc@outlook.com>
2024-03-03 15:28:13 +0800

    chore: remove unused opencv2/imgcodecs.hpp include

--- fastdeploy/vision/ocr/ppocr/utils/ocr_postprocess_op.h
+++ fastdeploy/vision/ocr/ppocr/utils/ocr_postprocess_op.h
@@ -20,7 +20,7 @@
 #include <ostream>
 #include <vector>
 #include "opencv2/core.hpp"
-#include "opencv2/imgcodecs.hpp"
+// #include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
 
 #include <cstring>
